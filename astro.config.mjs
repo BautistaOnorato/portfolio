1825d5718263b14bd7cf783b0f6f8f25a3d7defe
BautistaOnorato <bautistaonorato27@gmail.com>
2025-05-16 19:24:57 -0300

    output back to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   adapter: vercel(),
   integrations: [tailwind(), react(), compress()],
   i18n: {
