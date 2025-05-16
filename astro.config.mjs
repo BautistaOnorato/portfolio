97b91ffb83716390512247c1e48776d50af9b6ac
BautistaOnorato <bautistaonorato27@gmail.com>
2025-05-16 19:06:11 -0300

    enable ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import compress from "astro-compress";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [tailwind(), react(), compress()],
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "es",
   },
-  site: "https://bautistaonorato.vercel.app/"
 });
