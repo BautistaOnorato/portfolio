10371f5f7fb31c1a96b5f77f5f830aad24ea7098
BautistaOnorato <bautistaonorato27@gmail.com>
2025-05-19 14:06:36 -0300

    added google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import compress from "astro-compress";
-import vercel from '@astrojs/vercel/serverless';
+import partytown from "@astrojs/partytown";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  site: "https://bautistaonorato.vercel.app/",
+  output: "server",
   adapter: vercel(),
-  integrations: [tailwind(), react(), compress()],
+  integrations: [tailwind(), react(), compress(), partytown({ config: { forward: ["dataLayer.push"] } })],
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "es",
